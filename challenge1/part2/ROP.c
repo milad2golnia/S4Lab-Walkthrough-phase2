be84a972f9d21cfef2ab193b9bf93e73dcd46145
Milad Golgolnia <golgolnia@sharif.edu>
2022-02-19 12:34:39 +0330

    1. Challenge1, part2 solved!
    NEXT To Do: Add documentation of part2.

---
+++ challenge1/part2/ROP.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char bin_sh[10];
+FILE *fp;
+
+void correct_answer(int name1)
+{
+    if (name1 == 0xdabbadaa)
+    {
+        strcpy(bin_sh, "/bin");
+        printf("Noooo!\n");
+    }
+
+}
+
+void wrong_answer(int name1, int name2, int name3)
+{
+    if (name1 == 0xfacebaad || name2 == 0xfacefeed || name3 == 0xfacedead)
+    {
+        strcpy(bin_sh+4, "//sh");
+        printf("Do you mean facebook? Yeees!\n");
+    }
+    else if(name1==0xD0D0CACA || name2==0xB105F00D)
+    {
+        strcpy(bin_sh, "/LOL");
+        printf("Do you Mark Zuckerberg?!\n");
+    }
+}
+
+void Access_Shell() 
+{
+    execve(bin_sh,0,0);
+}
+
+void read_file(char* name)
+{
+    char input[100];
+    fp = fopen ("input.txt", "r");
+    if(fp == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+    printf(" Hello %s :)) \n can you name one of the big five companies in information technology in U.S.? \n ",name);
+    printf("write your answer in input.txt before running this program :))\n");
+    fscanf(fp, "%s", input);
+}
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+      fprintf(stderr, "argc != 2\n");
+      exit(EXIT_FAILURE);
+    }
+    read_file(argv[1]);
+    return 0;
+}
+
